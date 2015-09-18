7bec5fd1e1ad233657e40ae080f08b47b953ef04
Guest User <student@epicodus.com>
2015-09-18 10:05:28 -0700

    Spec and constructor to create simple no topping pizza.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function Pizza(quantity, pizzaSize, meats, veggies) {
+  this.quantity = quantity;
+  this.pizzaSize = pizzaSize;
+  this.meats = meats;
+  this.veggies = veggies;
+  this.cost = 0;
+}
--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,9 @@
- 
+describe("pizza", function() {
+  it('creates 1 medium cheese pizza with no toppings', function() {
+    var cheesePizza = new Pizza(1,"M",0,0); // 1 pizza, Medium, 0 meat addins, 0 veggie addins
+    expect(cheesePizza.quantity).to.equal(1);
+    expect(cheesePizza.pizzaSize).to.equal("M");
+    expect(cheesePizza.meats).to.equal(0);
+    expect(cheesePizza.veggies).to.equal(0);
+  });
+});
